e2cefca816cb4468c023e0da81da05e88dc3f9d8
lysiandon <don.lysiane@gmail.com>
2023-08-21 14:38:46 +0200

    created book routes

---
+++ backend/controllers/book.js
@@ -0,0 +1,89 @@
+const Book = require("../models/Book");
+const fs = require('fs');
+
+exports.getBooks = (_req,res) => {
+    Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json(error));
+};
+
+exports.getBookByID = (req,res) => {
+    Book.findById(req.params.id)
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(400).json(error));
+};
+
+exports.getBestRatedBooks =  async (req,res) => {
+    try {
+        // const topRatedBooks = await Book.find().sort((a,b) => Number(b.averageRating) - Number(a.averageRating)).splice(0,4);
+        const topRatedBooks = await Book.find().sort({averageRating: -1}).limit(3);
+        res.status(200).json(topRatedBooks); 
+    } catch (error) {
+        res.status(400).json(error);
+    }    
+};
+
+exports.createBook = (req,res) => {
+    const bookObj = JSON.parse(req.body.book);
+    const book = new Book({
+        ...bookObj,
+        userId: req.auth.userId,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+      });
+      book.save()
+        .then(book => res.status(201).json(book))
+        .catch(error => res.status(400).json(error));
+};
+
+exports.updateBook = (req, res) => {
+   const bookObj = req.file ? 
+   {...JSON.parse(req.body.book),imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`, userId: req.auth.userId} 
+   : 
+   {...req.body, userId: req.auth.userId};
+
+   Book.findById(req.params.id)
+   .then(book => {
+       if (book.userId != req.auth.userId) {
+           res.status(401).json({message: "Not authorized"})
+        } else {
+            Book.updateOne({_id: book._id}, bookObj)
+            .then(book => res.status(200).json(book))
+            .catch(error => res.status(401).json(error));
+    }
+   })
+   .catch(error => res.status(400).json(error))
+};
+
+exports.deleteBook = (req, res) => {
+    Book.findById(req.params.id)
+    .then(book => { 
+        if (book.userId != req.auth.userId) {
+            return res.status(401).json({ error: true, message: "Unauthorized" }); 
+        }
+        const filename = book.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+            Book.findByIdAndDelete(req.params.id)
+                .then(() => res.status(200).json({success: true, message: 'Objet supprimé !'}))
+                .catch(error => res.status(401).json({ error }));
+        });
+    })
+    .catch(error => res.status(500).json(error));
+};
+
+exports.addRating = (req, res) => {
+    Book.findById(req.params.id)
+    .then(book => {
+        //Make sure the user can't vote several times
+        book.ratings = book.ratings.filter(rating => rating.userId != req.auth.userId);
+        book.ratings.push({userId: req.auth.userId, grade: req.body.rating});
+
+        const sumRatings = book.ratings.reduce((sum, rating) => sum + rating.grade, 0);
+        const averageRating = sumRatings / book.ratings.length;
+        Book.findByIdAndUpdate({_id: book._id}, {ratings: book.ratings, averageRating}, {new: true})
+        .then(updatedBook => res.status(200).json(updatedBook))
+        .catch(error => res.status(401).json(error));
+    })
+    .catch(error => res.status(400).json({ error }));
+};
+
+
