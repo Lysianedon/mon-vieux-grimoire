44786d93bf9322601edbb3ce9020b57bf0388ed5
lysiandon <don.lysiane@gmail.com>
2023-09-16 23:39:09 +0200

    added sharp middleware

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -6,13 +6,13 @@ const Book = require("../models/Book");
 const bookCtrl = require("../controllers/book");
 //------------ MIDDLEWARE -----------------//
 const auth = require("../middlewares/auth");
-const multer = require("../middlewares/multer-config");
+const {multerUpload, sharpUpload} = require("../middlewares/upload-config");
 
 router.get('/', bookCtrl.getBooks);
 router.get("/bestrating", bookCtrl.getBestRatedBooks);
 router.get("/:id", bookCtrl.getBookByID);
-router.post("/", auth, multer, bookCtrl.createBook);
-router.put("/:id", auth, multer, bookCtrl.updateBook);
+router.post("/", auth, multerUpload, sharpUpload, bookCtrl.createBook);
+router.put("/:id", auth, multerUpload, sharpUpload, bookCtrl.updateBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
 router.post("/:id/rating", auth, bookCtrl.addRating);
 
