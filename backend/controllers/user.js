c7a9fbaa34ff9803d9d745502de0f0f64fa9897a
lysiandon <don.lysiane@gmail.com>
2023-09-17 01:25:48 +0200

    refactoring

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -6,43 +6,46 @@ dotenv.config({ path: "./config.env" });
 //------------ MODELS -----------------//
 const User = require("../models/User");
 
-exports.signup = (req,res) => {
-    bcrypt.hash(req.body.password, 10)
-    .then(hash => {
+exports.signup = async (req, res) => {
+    try {
+        const hash = await bcrypt.hash(req.body.password, 10);
         const user = new User({
             email: req.body.email,
             password: hash
         });
-        user.save()
-        .then(user => res.status(201).json(user))
-        .catch(error => res.status(400).json(error));
-    })
-    .catch(error => res.status(500).json(error))
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json(error);
+    }
 };
 
-exports.login = (req,res) => {
-    User.findOne({email: req.body.email})
-    .then(user => {
+exports.login = async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
         if (!user) {
-            return res.status(401).json({error: true, message: "email and/or password incorrect(s)"})
-        } else {
-            bcrypt.compare(req.body.password, user.password)
-            .then(isValid => {
-                if (!isValid) {
-                    return res.status(401).json({error: true, message: "email and/or password incorrect(s)"})
-                }
-                res.status(200).json({
-                    userId: user._id,
-                    token: jwt.sign(
-                        {userId: user._id},
-                        process.env.SECRET_KEY,
-                        {expiresIn: '24h'}
-                    )
-                })
-                
-            })
-            .catch(error => res.status(500).json(error))
+            return res
+                .status(401)
+                .json({ error: "email and/or password incorrect(s)" });
         }
-    })
-    .catch(error => res.status(500).json(error))
+
+        const isValid = await bcrypt.compare(req.body.password, user.password);
+
+        if (!isValid) {
+            return res
+                .status(401)
+                .json({ error: "email and/or password incorrect(s)" });
+        }
+
+        const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+            expiresIn: "24h"
+        });
+
+        res.status(200).json({
+            userId: user._id,
+            token
+        });
+    } catch (error) {
+        res.status(500).json(error);
+    }
 };
